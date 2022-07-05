ece0155954711110487e94bef8c0a39bbc0f1325
UdayReddy1446 <udayp1446@gmail.com>
2022-07-05 19:57:14 +0530

    first commit

---
+++ frond-end/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'product',pathMatch:'full'},
+  {path:'product',component:ProductComponent},
+  {path:'cart',component:CartComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frond-end/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FilterPipe } from './shared/filter.pipe';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductComponent,
+    FilterPipe,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
